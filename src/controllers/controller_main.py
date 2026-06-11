ef1c191b33682bb1a209b0f475b21d95c5331924
Danny <danny.v.der.pol@gmail.com>
2026-06-11 21:00:42 +0200

    add loading and saving main view position and size

--- src/controllers/controller_main.py
+++ src/controllers/controller_main.py
@@ -2,14 +2,45 @@
 Main controller.
 """
 
+import wx
+
+from src.models.application_settings import ApplicationSettings
 from src.views.view_frame_main import ViewFrameMain
 
 
 class ControllerMain:
 
     def __init__(self, title):
+        self._app_settings = ApplicationSettings()
         self._view = ViewFrameMain(title)
+        self._prepare_view()
         self._view.Show()
+
+    ###########
+    # Private #
+    ###########
+
+    def _prepare_view(self):
+        value = self._app_settings.get_main_window_position()
+        if -1 not in value:
+            self._view.SetPosition(value)
+        value = self._app_settings.get_main_window_size()
+        if -1 not in value:
+            self._view.SetSize(value)
+        self._view.Maximize(self._app_settings.get_main_window_maximized())
+
+        self._view.Bind(wx.EVT_CLOSE, self._on_view_close)
+
+    ##################
+    # Event handlers #
+    ##################
+
+    def _on_view_close(self, event):
+        self._app_settings.store_main_window_maximized(self._view.IsMaximized())
+        if not self._view.IsMaximized():
+            self._app_settings.store_main_window_position(*self._view.GetPosition())
+            self._app_settings.store_main_window_size(*self._view.GetSize())
+        event.Skip()
 
 
 if __name__ == "__main__":
