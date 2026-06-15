e5fb565ef8e954ffe9aa3aa9290572e8d24a95b8
Danny <danny.v.der.pol@gmail.com>
2026-06-15 08:17:44 +0200

    add test process name on process creation

--- processes/base/process_base.py
+++ processes/base/process_base.py
@@ -7,5 +7,23 @@ from abc import ABC
 
 class ProcessBase(ABC):
 
-    id = ""
     name = ""
+
+
+    def __init_subclass__(cls, **kwargs):
+        super().__init_subclass__(**kwargs)
+        # Process name
+        if cls.name is ProcessBase.name or not cls.name:
+            raise ValueError(
+                f"(Process) Process name is not set in process {cls.__name__}"
+            )
+        if not isinstance(cls.name, str):
+            raise ValueError(
+                f"(Process) Process name is not a sring in process {cls.__name__}"
+            )
+
+if __name__ == "__main__":
+
+    from tests.unit_tests.processes_tests.process_base_test import ProcessBaseTest
+
+    ProcessBaseTest().run()
---
+++ tests/unit_tests/processes_tests/process_base_test.py
@@ -0,0 +1,60 @@
+"""
+Template for a test suite
+"""
+
+import os
+
+from importlib.util import module_from_spec
+from importlib.util import spec_from_file_location
+
+import src.app_data as AppData
+
+from tests.lib.test_suite import TestSuite
+
+
+class ProcessBaseTest(TestSuite):
+
+    _process_base = None
+
+    def _create_derived_class(self, params):
+        _ProcessTestClass = type("ProcessTest", (self._process_base, ), params)
+
+    def setup(self):
+        filename = os.path.join(AppData.PROCESSES_PATH, "base", "process_base.py")
+        name = os.path.basename(filename).split(".")[0]
+        self.log.debug(f"Import file: {filename}")
+        spec = spec_from_file_location(name, str(filename))
+        module = module_from_spec(spec)
+        spec.loader.exec_module(module)
+        self.log.debug(f"Module loaded: {module}")
+        self._process_base = getattr(module, "ProcessBase")
+
+    def test_create_derived_class(self):
+        test_params = [
+            (
+                {},
+                "(Process) Process name is not set in process ProcessTest"
+            ),
+            (
+                {"name": ""},
+                "(Process) Process name is not set in process ProcessTest"
+            ),
+            (
+                {"name": 123},
+                "(Process) Process name is not a sring in process ProcessTest"
+            )
+
+        ]
+        for params in test_params:
+            try:
+                self._create_derived_class(params[0])
+                self.fail("No exception was raise while one was expected")
+            except Exception as e:
+                self.log.debug("Exception raised as expected")
+                self.log.debug(f"Message: {e}")
+                self.fail_if(str(e) != params[1], f"Wrong message, expected: '{params[1]}'")
+
+
+if __name__ == "__main__":
+
+    ProcessBaseTest().run()
