306c04c31d0c1909c7e4814ee1ef9587bf3facf5
Danny <danny.v.der.pol@gmail.com>
2026-06-18 19:27:45 +0200

    add function for sanitizing names for file paths

--- src/models/os_specifics.py
+++ src/models/os_specifics.py
@@ -4,6 +4,7 @@ OS Specifics.
 
 import os
 import sys
+import re
 
 from pathlib import Path
 
@@ -22,8 +23,13 @@ def get_user_data_dir():
             extend_path = [".local", "share"]
     return os.path.join(Path.home(), *extend_path)
 
+def sanitize_path(name):
+    return re.sub(r'[<>:"/\\|?*]', '_', name)
+
 
 if __name__ =="__main__":
 
     print("User dir     :", get_user_dir())
     print("User data dir:", get_user_data_dir())
+    print("Folder name  :", sanitize_path(
+        "my/name\\that<contains>invalid:characters?for*a\"folder|name"))
