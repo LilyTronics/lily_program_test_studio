29e7547e7415ee4cc762849428d544b5a59b0957
Danny <danny.v.der.pol@gmail.com>
2026-06-19 11:50:12 +0200

    add start process thread

--- src/models/process_runner.py
+++ src/models/process_runner.py
@@ -2,11 +2,15 @@
 Runs the process for a work order.
 - Run the process for every serial number.
 - Run sequential or parallel.
+The process is run in a separate thread to make sure the GUI is not freezing.
 """
 
+import threading
 
 
 class ProcessRunner:
+
+    _thread = None
 
     def __init__(self):
         raise Exception("This class should not be instantiated")
@@ -15,6 +19,13 @@ class ProcessRunner:
     # Private #
     ###########
 
+    @classmethod
+    def _check_settings(cls, settings):
+        pass
+
+    @classmethod
+    def _process_thread(cls, settings):
+        print(settings)
 
 
     ##########
@@ -23,8 +34,16 @@ class ProcessRunner:
 
     @classmethod
     def run_process(cls, settings):
-        print(settings)
+        if cls.is_running():
+            raise Exception("A process is already running")
+        cls._check_settings(settings)
+        cls._thread = threading.Thread(target=cls._process_thread, args=(settings, ))
+        cls._thread.daemon = True
+        cls._thread.start()
 
+    @classmethod
+    def is_running(cls):
+        return cls._thread is not None and cls._thread.is_alive()
 
 
 if __name__ == "__main__":
