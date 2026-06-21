0241e77f638d558c590a90a2f64cd9ce16865e0a
Danny <danny.v.der.pol@gmail.com>
2026-06-21 22:22:47 +0200

    add check settings

--- src/models/process_runner.py
+++ src/models/process_runner.py
@@ -7,6 +7,8 @@ The process is run in a separate thread to make sure the GUI is not freezing.
 
 import threading
 
+from src.models.process_registry import ProcessesRegistry
+
 
 class ProcessRunner:
 
@@ -21,12 +23,30 @@ class ProcessRunner:
 
     @classmethod
     def _check_settings(cls, settings):
-        pass
+        if "work_order" not in settings or not settings["work_order"]:
+            raise ValueError("The work order is not defined")
+        if not isinstance(settings["work_order"], str):
+            raise ValueError("The work order must be a string")
+        if "process" not in settings or not settings["process"]:
+            raise ValueError("The process is not defined")
+        if not isinstance(settings["process"], str):
+            raise ValueError("The process must be a string")
+        if not ProcessesRegistry.get_process(settings["process"]):
+            raise ValueError("The process does not exist")
+        if "serial_numbers" not in settings or not settings["serial_numbers"]:
+            raise ValueError("The serial numbers are not defined")
+        if not isinstance(settings["serial_numbers"], list):
+            raise ValueError("The serial numbers must be a list")
+        if not all(isinstance(s, str) for s in settings["serial_numbers"]):
+            raise ValueError("Not all the serial numbers are strings")
+        if "output_folder" not in settings or not settings["output_folder"]:
+            raise ValueError("The output folder is not defined")
+        if not isinstance(settings["output_folder"], str):
+            raise ValueError("The output folder must be a string")
 
     @classmethod
     def _process_thread(cls, settings):
         print(settings)
-
 
     ##########
     # Public #
--- tests/unit_tests/model_tests/process_runner_test.py
+++ tests/unit_tests/model_tests/process_runner_test.py
@@ -2,6 +2,7 @@
 Unit test for the processes runner.
 """
 
+from src.models.process_registry import ProcessesRegistry
 from src.models.process_runner import ProcessRunner
 from src.models.work_order import WorkOrder
 from tests.lib.test_suite import TestSuite
@@ -10,7 +11,39 @@ from tests.test_files.test_file import get_path
 
 class ProcessRunnerTest(TestSuite):
 
-    def test_run_sequential(self):
+    def setup(self):
+        ProcessesRegistry.load()
+
+    def test_run_exceptions(self):
+        test_params = [
+            ({}, "The work order is not defined"),
+            ({"work_order": 1234}, "The work order must be a string"),
+            ({"work_order": ""}, "The work order is not defined"),
+            ({"work_order": "WO-TEST"}, "The process is not defined"),
+            ({"process": 1234}, "The process must be a string"),
+            ({"process": ""}, "The process is not defined"),
+            ({"process": "not existing process"}, "The process does not exist"),
+            ({"process": "Process test sequential"}, "The serial numbers are not defined"),
+            ({"serial_numbers": "SNR001,SNR002"}, "The serial numbers must be a list"),
+            ({"serial_numbers": []}, "The serial numbers are not defined"),
+            ({"serial_numbers": ["SNR001", 2]}, "Not all the serial numbers are strings"),
+            ({"serial_numbers": ["SNR001", "SNR002"]}, "The output folder is not defined"),
+            ({"output_folder": 1234}, "The output folder must be a string"),
+            ({"output_folder": ""}, "The output folder is not defined"),
+        ]
+        settings = {}
+        for params in test_params:
+            settings.update(params[0])
+            try:
+                self.log.debug(f"Test with settings: {settings}")
+                ProcessRunner.run_process(settings)
+                self.fail("No exception was raise while one was expected")
+            except Exception as e:
+                self.log.debug("Exception raised as expected")
+                self.log.debug(f"Message: {e}")
+                self.fail_if(str(e) != params[1], f"Wrong message, expected: '{params[1]}'")
+
+    def test_run_process(self):
         WorkOrder.read_from_file(get_path("work_order_sequential.json"))
         ProcessRunner.run_process({
             "work_order": WorkOrder.get_work_order(),
@@ -18,7 +51,6 @@ class ProcessRunnerTest(TestSuite):
             "serial_numbers": WorkOrder.get_serial_numbers(),
             "output_folder": WorkOrder.get_output_folder()
         })
-
 
 
 if __name__ == "__main__":
